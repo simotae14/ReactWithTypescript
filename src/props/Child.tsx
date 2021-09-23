b1c364f57d088029a5ea4c4002679344700874c1
Simone <startae14@gmail.com>
2021-09-23 15:33:27 +0200

    03: second and more secure method to add Types to Props using React.FC<Type> construct

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -5,3 +5,8 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
   return <div>{ color }</div>;
 };
+
+// Second method to define Props with Types in a proper way
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{ color }</div>;
+};
